a20d33eb825e0a0003d779c6ffe87d56caf7c465
Jelena Mirosavljevic <jeckodotcom@gmail.com>
2018-08-15 11:31:19 +0200

    first commit - migrating from bitbucket

---
+++ src/main/java/org/spring/service/PersonService.java
@@ -0,0 +1,15 @@
+package org.spring.service;
+
+import java.util.List;
+
+import org.spring.model.Person;
+
+public interface PersonService {
+	
+	public void addPerson(Person p);
+	public void updatePerson(Person p);
+	public List<Person> listPersons();
+	public Person getPersonById(int id);
+	public void removePerson(int id);
+
+}
